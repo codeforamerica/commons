f0e1aa485e881a8860278f62496df1c84855d359
Ryan Resella <ryan@codeforamerica.org>
2012-01-11 20:09:53 -0800

    Added request file

---
+++ lib/commons/request.rb
@@ -0,0 +1,17 @@
+module Commons
+  module Request
+    def get(path, options={})
+      request(:get, path, options)
+    end
+
+    private
+
+    def request(method, path, options)
+      response = connection.send(method) do |request|
+        request.url(path, options)
+        request.params['response'] = 'json'
+      end
+      response.body
+    end
+  end
+end
