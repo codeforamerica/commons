0aee291fbceea52d7393d880ac803d00f9d67eb7
Ryan Resella <ryan@codeforamerica.org>
2012-01-11 20:09:46 -0800

    Added connection file

---
+++ lib/commons/connection.rb
@@ -0,0 +1,27 @@
+require 'faraday_middleware'
+
+module Commons
+  module Connection
+    private
+
+ def connection(options={})
+    merged_options = faraday_options.merge({
+        :headers => {
+          'Accept' => "application/#{response}",
+          'User-Agent' => user_agent
+        },
+        :ssl => {:verify => false},
+        :url => endpoint
+      })
+
+      Faraday.new(merged_options) do |builder|
+        builder.use Faraday::Request::UrlEncoded
+        builder.use Faraday::Response::RaiseError
+        builder.use Faraday::Response::Rashify
+        builder.use Faraday::Response::Mashify
+        builder.use Faraday::Response::ParseJson
+        builder.adapter(Faraday.default_adapter)
+      end
+    end
+  end
+end
