e3f3d76c1548c60b61af30c138922a6cb487bb23
Ryan Resella <ryan@codeforamerica.org>
2012-01-11 20:26:41 -0800

    Add helper methods to helper spec

--- spec/helper.rb
+++ spec/helper.rb
@@ -2,3 +2,24 @@ require 'simplecov'
 SimpleCov.start
 require 'commons'
 require 'rspec'
+require 'webmock/rspec'
+
+def a_get(url)
+  a_request(:get, commons_url(url))
+end
+
+def stub_get(url)
+  stub_request(:get, commons_url(url))
+end
+
+def fixture_path
+  File.expand_path("../fixtures", __FILE__)
+end
+
+def fixture(file)
+  File.new(fixture_path + '/' + file)
+end
+
+def commons_url(url)
+  "http://marketplace.civiccommons.org/api/v1#{url}"
+end
