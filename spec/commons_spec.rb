041d15369c9bf3482db174ed47bd1490ea4e667e
Ryan Resella <ryan@codeforamerica.org>
2012-01-11 20:31:17 -0800

    Add additional spec

--- spec/commons_spec.rb
+++ spec/commons_spec.rb
@@ -1,6 +1,16 @@
 require 'helper'
 
 describe Commons do
+  after do
+    Commons.reset
+  end
+
+  describe ".respond_to?" do
+    it "should be true if method exists" do
+      Commons.respond_to?(:new, true).should be_true
+    end
+  end
+
   describe ".new" do
     it "should return a Commons::Client" do
       Commons.new.should be_a Commons::Client
