65d1503b08edd210dd7b6a00ec3ac6fb9a13fd24
Ryan Resella <ryan@codeforamerica.org>
2012-01-11 19:57:05 -0800

    Added commons.rb base file

--- lib/commons.rb
+++ lib/commons.rb
@@ -1,3 +1,23 @@
+require 'commons/configuration'
+
 module Commons
-  # Your code goes here...
+ extend Configuration
+  class << self
+    # Alias for Commons::Client.new
+    #
+    # @return [Commons::Client]
+    def new(options={})
+      Commons::Client.new(options)
+    end
+
+    # Delegate to Commons::Client.new
+    def method_missing(method, *args, &block)
+      return super unless new.respond_to?(method)
+      new.send(method, *args, &block)
+    end
+
+    def respond_to?(method, include_private=false)
+      new.respond_to?(method, include_private) || super(method, include_private)
+    end
+  end
 end
