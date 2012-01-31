77091af878718dc272e5bbbcfa1a692485e64180
Ryan Resella <ryan@codeforamerica.org>
2012-01-31 13:42:50 -0800

    Documented content call

--- lib/commons/client/content.rb
+++ lib/commons/client/content.rb
@@ -1,6 +1,14 @@
 module Commons
   class Client
     module Content
+      # Use this query to get the information about one product
+      #
+      # @param id [String] The id for the product number
+      # @param options [Hash] A customizable set of options.
+      # @return [Array<Hashie::Mash>]
+      # @see http://marketplace.civiccommons.org/api
+      # @example
+      #   Comments.content("13351")
       def content(id,options={})
         get("node/#{id}.json", options)
       end
