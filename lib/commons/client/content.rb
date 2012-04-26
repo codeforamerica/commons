4b68e5afbaf2796def125db917cd4b6708ab97f7
Ryan Resella <ryan@codeforamerica.org>
2012-04-25 22:14:04 -0700

    Added taxonomy_terms

--- lib/commons/client/content.rb
+++ lib/commons/client/content.rb
@@ -8,10 +8,23 @@ module Commons
       # @return [Array<Hashie::Mash>]
       # @see http://www.civiccommons.org/api
       # @example
-      #   Comments.content("13351")
+      #   Commons.content("13351")
       def content(id,options={})
         get("node/#{id}.json", options)
       end
+
+      # Use this query to get the Taxonomy Terms
+      #
+      # @param id [String] The id for Taxonomy Term
+      # @param options [Hash] A customizable set of options.
+      # @return [Array<Hashie::Mash>]
+      # @see http://www.civiccommons.org/api
+      # @example
+      #   Commons.content("13351")
+      def taxonomy_term(id,options={})
+        get("taxonomy_term/#{id}.json", options)
+      end
+
     end
   end
 end
